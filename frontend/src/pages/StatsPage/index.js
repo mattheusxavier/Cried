8b31574e2d4db098f943236dc7ccef58a883f257
mattheusxavier <mattheus.x.a@gmail.com>
2020-11-25 17:49:05 -0300

    Developing page stats

--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,13 +1,65 @@
 import React from 'react'
+import Header from '../../components/Header'
+import { Container } from 'react-bootstrap'
 
+import ShortenerService from '../../services/shortnerService'
+
+import { StatsBox, StatsBoxTitle, StatsContainer, StatsRow } from './styles'
+
+import { FaExclamationTriangle } from 'react-icons/fa'
 class StatsPage extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoading: false,
+            shortenedURL: {},
+            errorMessage: '',
+        }
     }
 
+    async componentDidMount() {
+        const { code } = this.props.match.params
+
+        try {
+            const service = new ShortenerService()
+            const shortenedURL = await service.getStats(code)
+
+            this.setState({ isLoading: false, shortenedURL })
+        } catch (error) {
+            this.setState({ isLoading: false, errorMessage: 'Ops, a URL solicitada não existe. :( '})
+        }
+    }
+    
     render() {
+        const {errorMessage, shortenedURL} = this.state
         return (
-            <p>Stats Page Pitu</p>
+            <Container>
+                <Header>Estatísticas: </Header>
+                {errorMessage ? (
+                    <StatsContainer className="text-center">
+                        <FaExclamationTriangle size="62px" color="#F8D7DA" />
+                        <p className="m-3">{errorMessage}</p>
+                        <a className="btn btn-primary" href="/">Encurtar nova URL</a>
+                    </StatsContainer>
+                ) : (
+                    <StatsContainer className="text-center">
+                        <p><b>http://pitu.tk{shortenedURL.code}</b></p>
+                        <p>Redireciona para <br/> {shortenedURL.url}</p>
+                        <StatsRow>
+                            <StatsBox>
+                                <b>{shortenedURL.hits}</b>
+                                <StatsBoxTitle>Visitas</StatsBoxTitle>
+                            </StatsBox>
+                            <StatsBox>
+                                <b>{shortenedURL.relativeDate}</b>
+                                <StatsBoxTitle>Última visita</StatsBoxTitle>
+                            </StatsBox>
+                        </StatsRow>
+                        <a className="btn btn-primary" href="/">Encurtar nova URL</a>
+                    </StatsContainer>
+                )}
+            </Container>
         )
     }
 }
