24ca9c52616fd22af343565f581554f3f71e96ea
Matheus Xavier <mattheus.x.a@gmail.com>
2020-11-26 00:01:30 -0300

    Stats page

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize("mysql://root:root@localhost:3306/pitu")
+const sequelize = new Sequelize("mysql://root:@localhost:3306/pitu")
 
 export default sequelize
